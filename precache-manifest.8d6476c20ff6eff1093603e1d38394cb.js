740ee72962d24c54cf5bc79d92f97492614fb178
mjachowdhury <study.computing2016@gmail.com>
2019-12-06 12:03:27 +0000

    Updates

---
+++ precache-manifest.8d6476c20ff6eff1093603e1d38394cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf403a352cd6dec266ebfa6c47be3974",
+    "url": "/mjacportfolio/index.html"
+  },
+  {
+    "revision": "6d2f22be06c9f7cf0d7a",
+    "url": "/mjacportfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "98d7f38ba12856f58e15",
+    "url": "/mjacportfolio/static/js/2.2b2762c3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mjacportfolio/static/js/2.2b2762c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d2f22be06c9f7cf0d7a",
+    "url": "/mjacportfolio/static/js/main.a5a042d9.chunk.js"
+  },
+  {
+    "revision": "d52a39c19df52a650911",
+    "url": "/mjacportfolio/static/js/runtime-main.0c670e7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mjacportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
